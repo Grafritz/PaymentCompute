a84a6f1929460c1d7531cfe111a8e37b580049b6
Grafritz <duverseau.jeanfritz@gmail.com>
2020-11-08 18:15:31 -0500

    Index Of Emplyee Controller Commit

---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Paycompute.Models;
+using Paycompute.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController: Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                ImageUrl = employee.ImageUrl,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                Designation = employee.Designation,
+                City = employee.City,
+                DateJoined = employee.DateJoined
+
+            }).ToList();
+            int pageSize = 4;
+            //return View(EmployeeListPagination<EmployeeIndexViewModel>.Create(employees, pageNumber ?? 1, pageSize));
+            return View(employees);
+        }
+    }
+}
