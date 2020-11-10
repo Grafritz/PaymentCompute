ca025eb1acdc4bc9a208da660fe3c53ee8de9cee
Grafritz <duverseau.jeanfritz@gmail.com>
2020-11-10 10:49:06 -0500

    Layout, Index-EmployeeController

--- Paycompute/Controllers/EmployeeController.cs
+++ Paycompute/Controllers/EmployeeController.cs
@@ -15,11 +15,11 @@ namespace Paycompute.Controllers
     public class EmployeeController: Controller
     {
         private readonly IEmployeeService _employeeService;
-        private readonly HostingEnvironment _hostingEnvironment;
-        public EmployeeController(IEmployeeService employeeService, HostingEnvironment hostingEnvironment)
+        //private readonly HostingEnvironment _hostingEnvironment;
+        public EmployeeController(IEmployeeService employeeService/*, HostingEnvironment hostingEnvironment*/)
         {
             _employeeService = employeeService;
-            _hostingEnvironment = hostingEnvironment;
+            //_hostingEnvironment = hostingEnvironment;
         }
 
         public IActionResult Index()
@@ -83,10 +83,10 @@ namespace Paycompute.Controllers
                     var fileName = Path.GetFileNameWithoutExtension(model.ImageUrl.FileName);
                     var extension = Path.GetExtension(model.ImageUrl.FileName);
                     //var webRootPath = _hostingEnvironment.WebRootPath;
-                    var webRootPath = _hostingEnvironment.ContentRootPath;
+                    //var webRootPath = _hostingEnvironment.ContentRootPath;
                     fileName = DateTime.UtcNow.ToString("yymmssfff") + fileName + extension;
-                    var path = Path.Combine(webRootPath, uploadDir, fileName);
-                    await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
+                    //var path = Path.Combine(webRootPath, uploadDir, fileName);
+                    //await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
                     employee.ImageUrl = "/" + uploadDir + "/" + fileName;
                 }
                 await _employeeService.CreateAsync(employee);
@@ -159,11 +159,11 @@ namespace Paycompute.Controllers
                     var uploadDir = @"images/employee";
                     var fileName = Path.GetFileNameWithoutExtension(model.ImageUrl.FileName);
                     var extension = Path.GetExtension(model.ImageUrl.FileName);
-                    var webRootPath = _hostingEnvironment.ContentRootPath;
+                    //var webRootPath = _hostingEnvironment.ContentRootPath;
                     //var webRootPath = _hostingEnvironment.WebRootPath;
                     fileName = DateTime.UtcNow.ToString("yymmssfff") + fileName + extension;
-                    var path = Path.Combine(webRootPath, uploadDir, fileName);
-                    await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
+                    //var path = Path.Combine(webRootPath, uploadDir, fileName);
+                    //await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
                     employee.ImageUrl = "/" + uploadDir + "/" + fileName;
                 }
                 await _employeeService.UpdateAsync(employee);
